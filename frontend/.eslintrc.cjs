0c82459ced725dad7efb01c7729be7be260222ba
Devansh Baghel <devanshbaghel85@gmail.com>
2024-01-09 18:23:32 +0530

    changed frontend starter to ts + swc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+<<<<<<< HEAD
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
@@ -10,6 +11,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
+=======
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+>>>>>>> d953a95 (changed frontend starter to ts + swc)
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
