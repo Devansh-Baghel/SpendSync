528dba4be170d4bd83a18ac9ce5887161c0aa6d3
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-17 03:43:20 +0530

    feat: theme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import Routing from "./Routing";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
-  return <Routing />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* <ModeToggle /> */}
+      <Routing />
+    </ThemeProvider>
+  );
 }
 
 export default App;
