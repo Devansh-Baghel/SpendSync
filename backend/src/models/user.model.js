9603ad4083abf91c92a4f94daed8142e3d7bae71
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-16 10:24:14 +0530

    feat: user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    transactionHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+
+export const User = mongoose.model("User", userSchema);
