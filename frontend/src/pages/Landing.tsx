b82ba3809ab9b3cb577f0bb754df108843279c24
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-16 13:55:51 +0530

    feat: add shadcn/ui + custom theme

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar"
+import { Button } from "@/components/ui/button"
 
 function Landing() {
   return (
     <>
     <Navbar />
-      Landing
+      <Button>Click Me</Button>
     </>
   )
 }
