e471d7fc8f30e9e368c4785d0dd009f0f9c9f8e4
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-21 17:44:06 +0530

    backend: remove console.logs in auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,14 +5,9 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
-    console.log(req);
-    console.log(req.cookies);
-
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
-    console.log(token);
 
     if (!token) throw new ApiError(401, "Unauthorized request");
 
