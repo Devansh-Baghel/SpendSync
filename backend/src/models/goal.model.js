e4bae7fe5f59fa58491990614d1de0172f756296
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-23 22:37:31 +0530

    backend: add goal model

---
+++ backend/src/models/goal.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const goalSchema = mongoose.Schema(
+  {
+    finalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    currentAmount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    madeBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: {
+      type: String,
+      default: "other",
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Goal = mongoose.model("Goal", goalSchema);
