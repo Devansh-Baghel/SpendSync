a90db7e59712865c12c11e46518b6d3bdb1d5b1c
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-24 21:15:56 +0530

    frontend: basic goals display ui

---
+++ frontend/src/components/GoalsDisplay.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Badge } from "@/components/ui/badge"
+import { Progress } from "@/components/ui/progress"
+import { Button } from "./ui/button";
+
+function GoalsDisplay() {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    axios.get("/goals/get-goals").then((res) => {
+      setGoals(res.data.data.goals);
+      console.log(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="flex gap-6">
+    <ScrollArea className="h-[75vh] w-[500px] rounded-xl">
+      {goals.map((goal) => (
+        <Card className="mt-4">
+          <CardHeader>
+            <CardTitle className="text-lg">{goal.title}</CardTitle>
+            <CardDescription><Badge className="text-xs">{goal.category}</Badge></CardDescription>
+          </CardHeader>
+          <CardContent>
+            {/* <p>Current Amount: ${goal.currentAmount}</p> */}
+            {/* <p>Final Amount: ${goal.finalAmount}</p> */}
+            <div className="flex justify-center items-center gap-6 font-semibold">
+              $728
+              <Progress value={56} />
+              $1300
+            </div>
+          </CardContent>
+          {/* <CardFooter> */}
+          {/*   <p></p> */}
+          {/* </CardFooter> */}
+        </Card>
+      ))}
+    </ScrollArea>
+
+    <div className="flex flex-col justify-end">
+        <Card className="h-24 w-96 mb-2 flex items-center justify-center gap-6">
+          <Button>Create a New Goal</Button>
+          <Button>Add Money to a Goal</Button>
+        </Card>
+    </div>
+    </div>
+  );
+}
+
+export default GoalsDisplay;
--- frontend/src/pages/Goals.tsx
+++ frontend/src/pages/Goals.tsx
@@ -1,6 +1,7 @@
 import FirstGoal from "@/components/FirstGoal";
 import { useContext } from "react";
 import { AppContext } from "@/App";
+import GoalsDisplay from "@/components/GoalsDisplay";
 
 function Goals() {
   const { userData } = useContext(AppContext);
@@ -8,11 +9,7 @@ function Goals() {
   return (
     <div className="bg-primary rounded-[25px] w-screen h-[92vh] py-8 px-12">
       <h1 className="text-3xl font-bold text-background mb-5">Goals</h1>
-      {userData.user.goals.length === 0 ? (
-        <FirstGoal />
-      ) : (
-        <h2>Here are your goals</h2>
-      )}
+      {userData.user.goals.length === 0 ? <FirstGoal /> : <GoalsDisplay />}
     </div>
   );
 }
