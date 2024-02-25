0eb8f65e5842ab3afcebb1ce233b162c10b81cbb
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-25 16:10:10 +0530

    backend: update goal controller w/ endpoint

--- backend/src/controllers/goal.controller.js
+++ backend/src/controllers/goal.controller.js
@@ -41,3 +41,32 @@ export const getGoals = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, { goals }, "Goals sent successfully"));
 });
+
+export const updateGoal = asyncHandler(async (req, res) => {
+  const { goalId, title, finalAmount, description, category } = req.body;
+  const user = req.user;
+
+  if (!goalId) throw new ApiError(400, "Goal id is required to update goal");
+
+  const updateFields = {};
+  if (category) updateFields.category = category;
+  if (description) updateFields.description = description;
+  if (finalAmount) updateFields.finalAmount = finalAmount;
+  if (title) updateFields.title = title;
+
+  const updatedGoal = await Goal.findByIdAndUpdate(goalId, updateFields, {
+    new: true,
+  });
+
+  if (!updatedGoal) throw new ApiError(404, "Goal doesn't exist");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user, goal: updatedGoal },
+        "Goal updated successfully",
+      ),
+    );
+});
--- backend/src/routes/goal.routes.js
+++ backend/src/routes/goal.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createGoal, getGoals } from "../controllers/goal.controller.js";
+import {
+  createGoal,
+  getGoals,
+  updateGoal,
+} from "../controllers/goal.controller.js";
 
 const router = Router();
 
 // secure routes
 router.route("/create-goal").post(verifyJWT, createGoal);
 router.route("/get-goals").get(verifyJWT, getGoals);
+router.route("/update-goal").post(verifyJWT, updateGoal);
 
 export default router;
