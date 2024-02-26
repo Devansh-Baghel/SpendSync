5900b065c3244581207239d655a898d9b7e653ac
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-26 21:56:01 +0530

    frontend: ui for first card of account page

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,12 +1,74 @@
 import { useContext } from "react";
 import { AppContext } from "@/App";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+const getAvatarFallback = (name: string) =>
+  name
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase())
+    .join("");
 
 function Account() {
   const { userData } = useContext(AppContext);
 
   return (
-    <div className="bg-primary rounded-[25px] w-screen h-[92vh] py-8 px-12">
+    <div className="bg-primary rounded-[25px] w-screen h-[92vh] py-8 px-12 flex flex-col">
       <h1 className="text-3xl font-bold text-background mb-5">Account</h1>
+      <div className="flex flex-1 gap-6">
+        <Card className="w-[450px] py-8 flex flex-col items-center gap-5">
+          <Avatar className="w-32 h-32">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="text-3xl">
+              {getAvatarFallback(userData.user.fullName)}
+            </AvatarFallback>
+          </Avatar>
+          {!userData.user.avatar ? (
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="avatar" className="tex-md">
+                Upload Avatar
+              </Label>
+              <Input
+                id="avatar"
+                type="file"
+                className="file:text-primary h-10 file:mt-1 cursor-pointer"
+              />
+            </div>
+          ) : (
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="avatar" className="text-md">
+                Change Avatar
+              </Label>
+              <Input
+                id="avatar"
+                type="file"
+                className="file:text-primary h-12 file:mt-2"
+              />
+            </div>
+          )}
+
+          <div className="w-[290px] flex flex-col gap-1.5">
+            <Label htmlFor="fullName" className="txt-md">
+              Full Name
+            </Label>
+            <Input type="text" id="fullName" placeholder="Jhon Doe" />
+          </div>
+
+          <div className="w-[290px] flex flex-col gap-1.5">
+            <Label htmlFor="bio" className="txt-md">
+              Bio
+            </Label>
+            <Textarea id="bio" placeholder="Tell us about yourself..."  className=""/>
+          </div>
+
+            <Button className="mt-2 self-center w-[290px]">Save Changes</Button>
+        </Card>
+        <Card>asfd</Card>
+      </div>
     </div>
   );
 }
