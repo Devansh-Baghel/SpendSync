c7d9b7d979297fa226b0e4cacb010982fd2bf3ae
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-29 04:50:22 +0530

    frontend: rename file AddMoneyWhenBalanceZero to InitialDeposit

--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { AppContext } from "@/App";
 import AccountBalance from "@/components/AccountBalance";
-import AddMoneyWhenBalanceZero from "@/components/AddMoneyWhenBalanceZero";
+import InitialDeposit from "@/components/InitialDeposit";
 import IncomeAndExpense from "@/components/IncomeAndExpense";
 import AddIncomeAndExpense from "@/components/AddIncomeAndExpense";
 
@@ -14,7 +14,7 @@ function Overview() {
     <div className="bg-primary rounded-[25px] w-screen h-[92vh] py-8 px-12">
       <h1 className="text-3xl font-bold text-background mb-5">Overview</h1>
       {userData.user.currentBalance === 0 ? (
-        <AddMoneyWhenBalanceZero />
+        <InitialDeposit />
       ) : (
         <AccountBalance />
       )}
