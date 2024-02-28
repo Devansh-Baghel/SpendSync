02a0eff6756d96fe46055d574a3335ff8f933c0c
Devansh Baghel <devanshbaghel85@gmail.com>
2024-02-29 01:01:02 +0530

    backend: add feedback model

---
+++ backend/src/models/feedback.model.js
@@ -0,0 +1,22 @@
+import { mongoose } from "mongoose";
+
+const feedbackSchema = mongoose.Schema(
+  {
+    madeBy: {
+      type: Types.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: String,
+      required: true,
+      enum: ["1-star", "2-star", "3-star", "4-star", "5-star"],
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Feedback = mongoose.model("Feedback", feedbackSchema);
