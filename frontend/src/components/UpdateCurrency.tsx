e6737ce8fd77333d3d8297f99ca0cbd38ec9a9d0
Devansh Baghel <devanshbaghel85@gmail.com>
2024-03-03 02:37:58 +0530

    frontend: updating currency works

---
+++ frontend/src/components/UpdateCurrency.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { FormEvent, useContext, useState } from "react";
+import { AppContext } from "@/App";
+
+function UpdateCurrency() {
+  const [currency, setCurrency] = useState<string>();
+  const { userData, setUserData } = useContext(AppContext);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!currency) {
+      toast.error("Currency is required");
+      return;
+    }
+
+    if (currency === userData.user?.currency) {
+      toast.error("Nothing changed", {
+        id: "nothing-changed",
+        icon: "📌",
+      });
+      return;
+    }
+
+    const toastPromise = axios
+      .post("/users/update-currency", { newCurrency: currency })
+      .then((res) => {
+        localStorage.setItem("userData", JSON.stringify(res.data.data));
+        setUserData(res.data.data);
+      });
+
+    toast.promise(toastPromise, {
+      loading: "Saving",
+      success: "Saved",
+      error: "Error when fetching",
+    });
+  }
+  return (
+    <Card className="max-w-[350px]">
+      <form onSubmit={handleSubmit}>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-xl">Change Currency</CardTitle>
+        </CardHeader>
+        <CardFooter className="flex gap-4">
+          <Select onValueChange={(item) => setCurrency(item)}>
+            <SelectTrigger className="h-9 data-[placeholder]:text-muted-foreground">
+              <SelectValue placeholder="Select Currency" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="$">$ USD</SelectItem>
+              <SelectItem value="€">€ EUR</SelectItem>
+              <SelectItem value="¥">¥ JPY</SelectItem>
+              <SelectItem value="₹">₹ INR</SelectItem>
+              <SelectItem value="A$">A$ AUD</SelectItem>
+              <SelectItem value="C$">C$ CAD</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Button className="" type="submit">
+            Save Changes
+          </Button>
+        </CardFooter>
+      </form>
+    </Card>
+  );
+}
+
+export default UpdateCurrency;
--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,13 +1,5 @@
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import UpdatePassword from "@/components/UpdatePassword";
-import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import UpdateCurrency from "@/components/UpdateCurrency";
 
 function Settings() {
   return (
@@ -16,30 +8,7 @@ function Settings() {
 
       <div className="flex flex-col gap-6">
         <UpdatePassword />
-        <Card className="max-w-[350px]">
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-xl">Change Currency</CardTitle>
-          </CardHeader>
-          <CardFooter className="flex gap-4">
-            <Select>
-              <SelectTrigger className="h-9 data-[placeholder]:text-muted-foreground">
-                <SelectValue placeholder="Select Currency" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="U. S. Doller">$ USD</SelectItem>
-                <SelectItem value="Euro">€ EUR</SelectItem>
-                <SelectItem value="Japanese Yen">¥ JPY</SelectItem>
-                <SelectItem value="Indian Rupee">₹ INR</SelectItem>
-                <SelectItem value="Australian Dollar">A$ AUD</SelectItem>
-                <SelectItem value="Canadian Dollar">C$ CAD</SelectItem>
-              </SelectContent>
-            </Select>
-
-            <Button className="" type="submit">
-              Save Changes
-            </Button>
-          </CardFooter>
-        </Card>
+        <UpdateCurrency />
       </div>
     </div>
   );
