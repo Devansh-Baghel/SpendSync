5cfeddebf5e92c75338ef5051e75eb283bddc33c
Devansh Baghel <devanshbaghel85@gmail.com>
2024-03-10 15:24:18 +0530

    chore: add another domain in cors options

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+    origin: [
+      process.env.CORS_ORIGIN,
+      "http://localhost:5173",
+      "https://spendsync.baghel.dev/",
+    ],
     credentials: true,
   })
 );
