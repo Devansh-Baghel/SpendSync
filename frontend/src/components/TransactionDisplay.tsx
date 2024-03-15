31051253d0df9a66fb2c374dbcca03be122aef6c
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-03-15 19:41:31 +0530

    frontend: loading skeletons for transactions table

--- frontend/src/components/TransactionDisplay.tsx
+++ frontend/src/components/TransactionDisplay.tsx
@@ -10,10 +10,10 @@ import {
 } from "@/components/ui/table";
 import { AppContext } from "@/App";
 import { ScrollArea } from "./ui/scroll-area";
-// import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { FaPlus as PlusIcon } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import TransactionSkeleton from "./TransactionSkeleton";
 
 const formatter = new Intl.NumberFormat("en-US");
 
@@ -30,11 +30,13 @@ function TransactionDisplay() {
   const [transactions, setTransactions] = useState([]);
   const { userData } = useContext(AppContext);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("/transaction/get-transactions").then((res) => {
       setTransactions(res.data.data.transactions.reverse());
       console.log(res.data);
+      setLoading(false);
     });
   }, []);
 
@@ -51,26 +53,31 @@ function TransactionDisplay() {
               <TableHead className="">Amount</TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody>
-            {transactions.map((transaction: TransactionType) => (
-              <TableRow key={transaction._id}>
-                <TableCell className="font-medium">
-                  {transaction.type}
-                </TableCell>
-                <TableCell>{transaction.wallet}</TableCell>
-                <TableCell>{transaction.title}</TableCell>
-                <TableCell className="">
-                  {transaction.type === "Expense" ? (
-                    <span>&minus; </span>
-                  ) : (
-                    <span>+ </span>
-                  )}
-                  {userData.user.currency}
-                  {formatter.format(transaction.amount)}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+
+          {loading ? (
+            <TransactionSkeleton />
+          ) : (
+            <TableBody>
+              {transactions.map((transaction: TransactionType) => (
+                <TableRow key={transaction._id}>
+                  <TableCell className="font-medium">
+                    {transaction.type}
+                  </TableCell>
+                  <TableCell>{transaction.wallet}</TableCell>
+                  <TableCell>{transaction.title}</TableCell>
+                  <TableCell className="">
+                    {transaction.type === "Expense" ? (
+                      <span>&minus; </span>
+                    ) : (
+                      <span>+ </span>
+                    )}
+                    {userData.user.currency}
+                    {formatter.format(transaction.amount)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          )}
         </Table>
       </ScrollArea>
 
