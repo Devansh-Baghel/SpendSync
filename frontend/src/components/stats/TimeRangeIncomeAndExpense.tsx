0f26fd612753478dc099c9101a4af3f6dd5f1083
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-05-10 14:56:42 +0530

    frontend: show accurate data by timerange

--- frontend/src/components/stats/TimeRangeIncomeAndExpense.tsx
+++ frontend/src/components/stats/TimeRangeIncomeAndExpense.tsx
@@ -2,27 +2,41 @@ import { Bar } from "react-chartjs-2";
 
 interface TimeRangeProps {
   timeRange: "week" | "month" | "year";
+  data: {
+    income: number;
+    expense: number;
+  };
 }
 
-function TimeRangeIncomeAndExpense({ timeRange }: TimeRangeProps) {
-  const data = {
+function TimeRangeIncomeAndExpense({ timeRange, data }: TimeRangeProps) {
+  const chartData = {
     labels: ["Income", "Expense"],
     datasets: [
       {
         label: `Income and expense for this ${timeRange}`,
-        data: [122, 123],
+        data: [data.income, data.expense],
         backgroundColor: ["#22c55e", "#ef4444"],
         hoverOffset: 4,
       },
     ],
   };
 
+  if (!data.income && !data.expense) {
+    return (
+      <div className="bg-card max-w-[300px] rounded-[20px] p-5 flex flex-col items-center gap-4">
+        <p className="font-medium text-center flex items-center">
+          You didn't make any transactions this {timeRange}.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-card max-w-[300px] rounded-[20px] p-5 flex flex-col items-center gap-4">
       <p className="font-medium text-center flex items-center">
         Income and expense for this {timeRange}
       </p>
-      <Bar data={data} />
+      <Bar data={chartData} />
     </div>
   );
 }
--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,14 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "@/App";
 import HiddenStats from "../components/stats/HiddenStats";
 import CheckoutButton from "../components/CheckoutButton";
 import useTitle from "../hooks/useTitle";
 import FixedIncomeExpenseGraph from "../components/stats/FixedIncomeExpenseGraph";
 import TimeRangeIncomeAndExpense from "../components/stats/TimeRangeIncomeAndExpense";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function Statistics() {
   useTitle("Statistics");
   const { userData } = useContext(AppContext);
+  const { data, isLoading } = useQuery({
+    queryKey: ["time-range-income-expense"],
+    queryFn: () => {
+      return axios
+        .get("/stats/get-income-expense-by-time-range")
+        .then((res) => res.data.data);
+    },
+  });
+
+  console.log(data);
 
   return (
     <div className="bg-primary rounded-[25px] md:w-screen py-5 px-5 sm:px-8 mt-[-100px] md:mt-0">
@@ -17,11 +29,16 @@ function Statistics() {
       {userData.user.isPaidUser ? (
         <div className="flex gap-4">
           <FixedIncomeExpenseGraph />
-          <div className="flex flex-wrap">
-            <TimeRangeIncomeAndExpense timeRange="week" />
-            <TimeRangeIncomeAndExpense timeRange="month" />
-            <TimeRangeIncomeAndExpense timeRange="year" />
-          </div>
+          {isLoading ? (
+            // TODO: Add loading skeletons here
+            <p>Data loading...</p>
+          ) : (
+            <div className="flex flex-wrap">
+              <TimeRangeIncomeAndExpense timeRange="week" data={data.week} />
+              <TimeRangeIncomeAndExpense timeRange="month" data={data.month} />
+              <TimeRangeIncomeAndExpense timeRange="year" data={data.year} />
+            </div>
+          )}
           {/* // TODO: Display stats */}
         </div>
       ) : (
